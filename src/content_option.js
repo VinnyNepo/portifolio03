85ba84147f73eeed01f9174c95b6e298e4e5fe13
Antonio Vinicius <127804721+VinnyNepo@users.noreply.github.com>
2023-07-14 11:17:30 -0300

    git add

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "VINICIUS";
+const meta = {
+    title: "Vinicius Neponuceno",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "Vinicius Neponuceno",
+    animated: {
+        first: "Sou Estudante de ADS",
+        second: "Iniciando na area em 2023",
+        third: "eu amo programar",
+    },
+    description: "Estou em busca de realizar um sonhor, trabalhar na area da tecnologia",
+    your_img_url: "https://img.freepik.com/fotos-premium/praia-preta-e-branca-perfeita-paisagem-dramatica-da-costa-da-ilha-ceu-escuro-monocromatico-de-agua-calma_663265-1715.jpg",
+};
+
+const dataabout = {
+    title: "Um pouco sobre mim mesmo",
+    aboutme: " Estou iniciando minha carreira, pretendo me especializar na area da programação, buscar sempre dar meu maximo",
+};
+const worktimeline = [{
+        jobtitle: "Repositorio GitHub",
+        where: "Unifasipe",
+        date: "2023",
+    },
+    {
+        jobtitle: "Cybersecurity",
+        where: "Unifasipe",
+        date: "2023",
+    },
+    {
+        jobtitle: "Design Grafico",
+        where: "Jr Info",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 20,
+    },
+    {
+        name: "Html",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 20,
+    },
+    {
+        name: "React",
+        value: 30,
+    },
+    {
+        name: "CSS",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "",
+        description: "",
+    },
+    {
+        title: "",
+        description: "",
+    },
+    {
+        title: "",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vinycius0neponuceno@gmail.com",
+    YOUR_FONE: "(99) 98470-0084",
+    description: "Entre em contato comigo ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "gmail",
+    YOUR_TEMPLATE_ID: "vinny",
+    YOUR_USER_ID: "pbJ9zGM37egr6e2fWHqc-",
+};
+
+const socialprofils = {
+    github: "https://github.com/VinnyNepo",
+    facebook: "https://www.facebook.com/vinicius.neponuceno.16",
+    linkedin: "https://www.linkedin.com/in/antonio-vinicius-lima-neponuceno-b3487022b/",
+    instagram: "https://www.instagram.com/vinycius_neponuceno/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
